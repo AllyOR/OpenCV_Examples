3b03d25fe26e82acfa2040ebaa311f62d64b702c
Alison Orellana <ali15.orellan@gmail.com>
2020-05-20 12:13:59 -0400

    refactorized

--- ColorDetector/Color.py
+++ ColorDetector/Color.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 
-src = cv2.imread("images/imagen.png")
+src = cv2.imread("images/image.png")
 exit = np.uint16(src)
+
 # Defining the range of red color (the dark and light red)
 #dark red
 hsv_min_red = np.array([0, 100, 100]) 
@@ -14,9 +15,11 @@ hsv_max_red2 = np.array([179, 255, 255])
 # Applying the Gaussian smoothing function
 gauss = cv2.GaussianBlur(src, (5,5), 0)
 cv2.imwrite("images/Smooth.jpg", gauss)
+
 # Convert to HSV to get as much background as possible
 hsv_red = cv2.cvtColor(gauss, cv2.COLOR_BGR2HSV)
 cv2.imwrite("images/HSV.jpg", hsv_red)
+
 # Create the masks for the red color
 dark_red = cv2.inRange(hsv_red, hsv_min_red, hsv_max_red)
 light_red = cv2.inRange(hsv_red, hsv_min_red2, hsv_max_red2)
@@ -32,3 +35,4 @@ cv2.imshow("Tones of red", masks_red)
 cv2.imshow("Exit", res)
 
 print("======END=======")
+
